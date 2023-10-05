5aa847d90015fc811252c0db026f2333bd1a2217
Sam-Ridgwell <sam.ridgwell@theaccessgroup.com>
2023-10-05 02:14:59 +0100

    Added in core, and unit tests for Results object.

---
+++ Contacts.Core.Test/ResultTest.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts.Core.Test
+{
+    public class ResultTests
+    {
+        [Fact]
+        public void TestGetValue_Success()
+        {
+            //Arrange
+            Result<string> result = new Result<string>("Test");
+
+            //Act
+            var value = result.GetValue();
+
+            //Assert
+            Assert.Equal("Test", value);
+        }
+
+        [Fact]
+        public void TestGetValue_Failure()
+        {
+            //Arrange
+            Result<string> result = new Result<string>(new Exception("Test"));
+
+            //Act
+            var value = result.GetValue();
+
+            //Assert
+            Assert.Equal(default(string), value);
+        }
+
+        [Fact]
+        public void TestSetValue_Success()
+        {
+            //Arrange
+            Result<int> result = new Result<int>(0);
+
+            //Act
+            result.SetValue(10);
+
+            //Assert
+            Assert.Equal(10, result.GetValue());
+        }
+
+        [Fact]
+        public void TestSetValue_Failure()
+        {
+            //Arrange
+            Result<int> result = new Result<int>(new Exception("Test"));
+
+            //Act
+            result.SetValue(10);
+
+            //Assert
+            Assert.Equal(10, result.GetValue());
+        }
+
+        [Fact]
+        public void TestIsSuccess_Success()
+        {
+            //Arrange
+            Result<bool> result = new Result<bool>(true);
+
+            //Act
+            var isSuccess = result.IsSuccess();
+
+            //Assert
+            Assert.Equal(true, isSuccess);
+        }
+
+        [Fact]
+        public void TestIsSuccess_Failure()
+        {
+            //Arrange
+            Result<bool> result = new Result<bool>(new Exception("Test"));
+
+            //Act
+            var isSuccess = result.IsSuccess();
+
+            //Assert
+            Assert.Equal(false, isSuccess);
+        }
+
+        [Fact]
+        public void TestGetException_Success()
+        {
+            //Arrange
+            Result<string> result = new Result<string>("Test");
+
+            //Act
+            var exception = result.GetException();
+
+            //Assert
+            Assert.Equal(null, exception);
+        }
+
+        [Fact]
+        public void TestGetException_Failure()
+        {
+            //Arrange
+            Result<string> result = new Result<string>(new Exception("Test"));
+
+            //Act
+            var exception = result.GetException();
+
+            //Assert
+            Assert.Equal("Test", exception?.Message);
+        }
+
+        [Fact]
+        public void TestSetException_Success()
+        {
+            //Arrange
+            Result<int> result = new Result<int>(5);
+
+            //Act
+            result.SetException(new Exception("Test"));
+
+            //Assert
+            Assert.Equal(false, result.IsSuccess());
+        }
+
+        [Fact]
+        public void TestSetException_Failure()
+        {
+            //Arrange
+            Result<int> result = new Result<int>(new Exception("Test"));
+
+            //Act
+            result.SetException(new Exception("New Test"));
+
+            //Assert
+            Assert.Equal("New Test", result.GetException()?.Message);
+        }
+    }
+}
---
+++ Contacts.Core/Result.cs
@@ -0,0 +1,87 @@
+﻿namespace Contacts.Core
+{
+    public class Result<T>
+    {
+        private T? _value;
+        private Exception? _exception = null;
+        private bool _isSuccess = false;
+
+        /// <summary>
+        /// Creates a result with a value
+        /// </summary>
+        /// <param name="value"></param>
+        public Result(T value)
+        {
+            _value = value;
+            _isSuccess = true;
+            _exception = null;
+        }
+
+        /// <summary>
+        /// Creates a result with an exception
+        /// </summary>
+        /// <param name="ex"></param>
+        public Result(Exception ex)
+        {
+            _isSuccess = false;
+            _exception = ex;
+            _value = default;
+        }
+
+        /// <summary>
+        /// Gets the value if the result is successful
+        /// </summary>
+        /// <returns></returns>
+        public T? GetValue()
+        {
+            if (_isSuccess && _value != null)
+                return _value;
+
+            return default;
+        }
+
+        /// <summary>
+        /// Sets the value and sets the result to true
+        /// </summary>
+        /// <param name="value"></param>
+        public void SetValue(T value)
+        {
+            _value = value;
+            _isSuccess = true;
+            _exception = null;
+        }
+
+        /// <summary>
+        /// Returns true if the result is successful
+        /// </summary>
+        /// <returns></returns>
+        public bool IsSuccess()
+        {
+            return _isSuccess;
+        }
+
+        /// <summary>
+        /// Gets the exception if the result is not successful
+        /// </summary>
+        /// <returns></returns>
+        public Exception? GetException()
+        {
+            if (_exception != null && !_isSuccess)
+                return _exception;
+
+            return null;
+        }
+
+        /// <summary>
+        /// Sets the exception and sets the result to false
+        /// </summary>
+        /// <param name="ex"></param>
+        public void SetException(Exception ex)
+        {
+            _exception = ex;
+            _isSuccess = false;
+            _value = default;
+        }
+    }
+    
+}
